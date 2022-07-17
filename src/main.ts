be0df7250c62643fec6802724846a3d9e266e3a5
Pavel <bypavelsnigirev@gmail.com>
2022-07-17 14:54:42 +0200

    feat: update pipe and delete comments

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,7 @@ import { writeFile } from 'fs/promises';
 async function bootstrap() {
   const port = process.env.PORT || 4001;
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
-  // const swagger_doc = await readFile(
-  //   join(dirname(__dirname), 'doc', 'api.yaml'),
-  //   'utf-8',
-  // );
-  // const document = parse(swagger_doc);
-  //
-  // SwaggerModule.setup('doc', app, document);
-  ////////////////////////////////////////////////////
-  // const config = new DocumentBuilder()
-  //   .setTitle('Basic service')
-  //   .setDescription('The basic API description')
-  //   .setVersion('1.0')
-  //   .addTag('basic')
-  //   .setBasePath('doc/api.yaml')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('doc', app, document);
-  ///////////////////////////////////////////////////
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const options = new DocumentBuilder()
     .setTitle('Basic service')
     .setDescription('Basic api')
